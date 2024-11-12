8dadf6d36bde5a9fa0b9572d5d7e466376f2e372
Marcheline <you@example.com>
2024-11-12 19:23:18 +0100

    formu

--- pokemonsAnadidos.js
+++ pokemonsAnadidos.js
@@ -4,10 +4,14 @@ pokemonInfo = pokemonInfo.replace('?','');
 let pokemonInfos = pokemonInfo.split('&');
 let pokemonId = 0;
 let pokemonAux = 0;
-for (let i in pokemon) {
-    pokemonAux = pokemon[i].id;
-    if(pokemonAux>pokemonId){
-        pokemonId = pokemonAux;
+if(pokemonsMas.length>1){
+    
+}else{
+    for (let i in pokemon) {
+        pokemonAux = pokemon[i].id;
+        if(pokemonAux>pokemonId){
+            pokemonId = pokemonAux;
+        }
     }
 }
 let nombre = pokemonInfos[0].split('=')[1];
@@ -16,5 +20,5 @@ let tipo2 = pokemonInfos[2].split('=')[1];
 let tipos = [];
 tipos.push(tipo1);
 tipos.push(tipo2);
-let pokeNuevo = {"id":pokemonId,"nombre":nombre,"tipos":tipos};
+let pokeNuevo = {"id":pokemonId+1,"nombre":nombre,"tipos":tipos};
 console.log(pokeNuevo);
